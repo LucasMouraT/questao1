4dcc953fa446a4bdedf87b33d2eccc3afa0021d9
LucasMouraT <120618552+LucasMouraT@users.noreply.github.com>
2024-09-06 16:07:18 -0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,8 @@
+string_original = "lucas"
+
+string_invertida = ""
+for caractere in string_original:
+    string_invertida = caractere + string_invertida
+    
+print("String original:", string_original)
+print("String invertida:", string_invertida)
